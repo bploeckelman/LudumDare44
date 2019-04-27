c0b5cd6b521abae7a798fb9257e04928050a5c7f
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2019-04-27 10:06:18 -0500

    Initial commit

---
+++ desktop/src/lando/systems/ld44/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package lando.systems.ld44.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import lando.systems.ld44.LudumDare44;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LudumDare44(), config);
+	}
+}
